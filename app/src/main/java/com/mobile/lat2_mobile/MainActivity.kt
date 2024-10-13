62246142b4512784418337cbf54a8a26460709ce
Desvita Putri <161610929+desvitaputri03@users.noreply.github.com>
2024-10-13 22:09:28 +0700

    Latihan 2 Mobile

---
+++ app/src/main/java/com/mobile/lat2_mobile/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.mobile.lat2_mobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var etUsername: TextView
+    private lateinit var etPassword: TextView
+    private lateinit var btnLogin: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        etUsername = findViewById(R.id.user)
+        etPassword = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.submitHere)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnLogin.setOnClickListener() {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            btnLogin.setOnClickListener() {
+                val username = etUsername.text.toString()
+                val password = etPassword.text.toString()
+
+                if (username == "admin" && password == "123456") {
+                    val intent = Intent(this@MainActivity,MobileApp::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "username atau password salah", Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+    }
+}
